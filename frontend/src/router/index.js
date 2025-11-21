da7f0db84ff111f8de33bf6575b5a21d04f52010
Irmansyah Muslimin <117552270+kiraascoder@users.noreply.github.com>
2025-11-21 20:26:53 +0800

    Update index.js

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,10 +1,22 @@
 import { createRouter, createWebHistory } from "vue-router";
-import EventView from "../views/EventView.vue";
 import DashboardView from "../views/DashboardView.vue";
+import EventListView from "../views/EventListView.vue";
+// PricingView, EventFormView nanti kita tambahkan
 
 const routes = [
-  { path: "/", name: "dashboard", component: DashboardView },
-  { path: "/events", name: "events", component: EventView },
+  {
+    path: "/",
+    name: "dashboard",
+    component: DashboardView,
+  },
+  {
+    path: "/events",
+    name: "events.index",
+    component: EventListView,
+  },
+  // nanti:
+  // { path: "/events/new", name: "events.create", component: EventFormView },
+  // { path: "/events/:id/pricing", name: "events.pricing", component: PricingView, props: true },
 ];
 
 const router = createRouter({
