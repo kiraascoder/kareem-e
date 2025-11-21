70b8595e1f1041317fb3c85f318e84c647324aa1
Irmansyah Muslimin <117552270+kiraascoder@users.noreply.github.com>
2025-11-21 20:25:08 +0800

    Update main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,16 +1,8 @@
-import axios from "axios";
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-
-const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:8000/api",
-});
 
 const app = createApp(App);
 
 app.use(router);
 app.mount("#app");
-
-export default api;
-
